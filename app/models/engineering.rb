a4ad2caf61c47d44ea2167579f96bf32693052d7
edisaurus <edswork.ece@gmail.com>
2021-02-23 17:09:34 +0800

    create our own blog app activity

---
+++ app/models/engineering.rb
@@ -0,0 +1,5 @@
+class Engineering < ApplicationRecord
+    validates :subject, :description, :units, presence: true
+    validates :description, length: { minimum: 10, maximum: 120 }
+    validates :units, numericality: { only_integer: true }
+end
